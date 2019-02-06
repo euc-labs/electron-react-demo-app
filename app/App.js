9a230c7e7e07dc49c607b28d7a07136447463fc7
Julian Bonomini <julian.bonomini@gmail.com>
2019-02-05 21:55:21 -0300

    statless component

--- app/App.js
+++ app/App.js
@@ -1,15 +1,5 @@
 import React, { Component } from 'react';
 
-class AppRouter extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-    }
-  }
-
-  render() {
-    return <h1>Hello world!</h1>
-  }
-}
+const AppRouter = () => <h1>Hello world!</h1>
 
 export default AppRouter;
